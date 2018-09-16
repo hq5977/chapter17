26dea90c51fb6336f050af033199476b05f14da2
黄强 <QiangHuang@k2workflow.com.cn>
2018-09-16 23:12:18 +0800

    init

---
+++ src/main/java/com/smart/web/UserController.java
@@ -0,0 +1,34 @@
+package com.smart.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.smart.UserService;
+import com.smart.domain.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView createUser(User user) {
+		userService.createUser(user);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("user/createSuccess");
+		mav.addObject("user", user);
+		return mav;
+	}
+	
+	@RequestMapping("/register")
+	public String register(){
+		
+		return "user/register";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/user/register.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>新增用户</title>
+</head>
+<body>
+	<form method="post" action="<c:url value="/user.html"/>">
+		<table>
+			<tr>
+				<td>用户名：</td>
+				<td><input type="text" name="userName"/></td>
+			</tr>
+			<tr>
+				<td>密码：</td>
+				<td><input type="password" name="passWord"/></td>
+			</tr>
+			<tr>
+				<td>姓名：</td>
+				<td><input type="text" name="realName"/></td>
+			</tr>
+			<tr>
+			<td colspan="2"> <input type="submit" name="提交" /></td>
+			</tr>
+				
+		
+	
+	</table>
+	
+	</form>
+</body>
+</html>
